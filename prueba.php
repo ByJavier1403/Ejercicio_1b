f8725082273a8293acbb12c090cb90fb789cee84
ByJavier1403 <javierdavid02@gmail.com>
2020-04-02 22:30:54 -0600

    Ejercicio 1b

---
+++ prueba.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="es">
+	<head>
+		<title>Walmart</title>
+		<link rel="stylesheet" href="estilos.css">
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scake=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	</head>
+	<body>
+		<img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fc%2Fca%2FWalmart_logo.svg%2F1280px-Walmart_logo.svg.png&f=1&nofb=1" alt="Logotipo de HTML5" width="200" height="75">
+		<div class="container">
+			<div class="card">
+				<img src="https://lh3.googleusercontent.com/proxy/FsZ1VPPXr4aH040Oj17nHTiZLfmVfEmLkB5vZN7tVB9Lyd19N-lPhPGBu7XqSh1mVbO2vozvPNggF5xJNJEht1793sQ5Qr7f2GieQo6NCWAPqOs">
+				<h4>Lámina de 8cm X 10 cm, Calibre 22<br/>$580.0</h4>
+				<a href="#">Comprar</a>
+			</div>
+			<div class="card">
+				<img src="https://ae01.alicdn.com/kf/HTB1d.NDSpXXXXXMaVXXq6xXFXXXB.jpg_q50.jpg">
+				<h4>Cordón flex 5 mts<br/>$380.0</h4>
+				<a href="#">Comprar</a>
+			</div>
+			<div class="card">
+				<img src="https://lh3.googleusercontent.com/proxy/Mh5RzdJbXTNGs4H5rV5k9kLTMvlI6hkWmcJO-2PDdVUCTGnU36ofL8cTJVIqbg9WQ1Xcr21KIzr6GV_rfoEGNAY0v5b9g9WHsohCef4f6SaTEI1CQNrmA4x5jNCK-8jC-DGz">
+				<h4>Led blanco brillante industrial 5v.<br/>$10.0</h4>
+				<a href="#">Comprar</a>
+			</div>
+		</div>
+		<?php
+			$productos = ['Lamina de 8cm X 10 cm, calibre 22', 'Cordon flex 5mts', 'Led blanco brillante industrial'];
+			$precio = [580,380,10];
+			$descuento = [0.05,0.03,0.02];
+		?>
+		<i><br/>Hernández Estrada Javier David 4AMP <br/> "Programa de facutración de un producto php y html</i>
+	</body>
+</html>
